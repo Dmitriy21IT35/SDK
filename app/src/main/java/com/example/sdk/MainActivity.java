31861eba7df05ca48cfb8d0fca265c0c9b84bc9c
Dima <dimaforevermelnik@gmail.com>
2022-03-18 10:39:00 +0300

    ДОБАВКА СТУДЕНТА

---
+++ app/src/main/java/com/example/sdk/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
